83b641cfa966ba816a9525be0e7ec0a1dad24bc0
Sris Moparthy <smopart@gmail.com>
2019-01-24 12:25:26 -0800

    updated loaderio config

--- proxy/index.js
+++ proxy/index.js
@@ -12,6 +12,11 @@ proxy.use(bodyParser.json());
 proxy.use(bodyParser.urlencoded({ extended: true }));
 proxy.use(cors());
 proxy.use(express.static(path.join(__dirname, './public/')));
+
+proxy.get('/loaderio-0e0f9e7bccd1041ec8f5e4008d9b990e.txt', function(req, res) {
+  res.sendFile("../public/loaderio-0e0f9e7bccd1041ec8f5e4008d9b990e.txt");
+});
+
 proxy.use('/api', router);
 
 proxy.listen(PORT, (err) => {
